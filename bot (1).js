b0e27616854aef3ee7e186fcc9db13298fe0edc1
ZoldeX-Sama <45310570+ZoldeX-Sama@users.noreply.github.com>
2018-12-01 21:13:19 +0300

    Add files via upload

---
+++ bot (1).js
@@ -0,0 +1,34 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix = "1";
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);  
+});
+
+client.on('ready', async() => {
+var server = "518481035063984129"; // ايدي السررفر
+var channel = "518487887847882752";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**spaaaaaaaaaaaaaaaammmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm**')
+    },305);
+})
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "sf") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
+
+ 
+client.login(process.env.BOT_TOKEN);
